3874429c1c8bd0f9eda4c5e25ee7d9f9b068aff1
启明 王 <martinkingwang@hotmail.com>
2019-06-10 17:19:32 +0800

    变更webhelper逻辑，使其符合服务器运行要求

--- OnlineDataCrawler/Program.cs
+++ OnlineDataCrawler/Program.cs
@@ -2,6 +2,7 @@
 using OnlineDataCrawler.Util;
 using OnlineDataCrawler.Engine;
 using System.Text;
+using System.Runtime.Loader;
 
 namespace OnlineDataCrawler
 {
@@ -9,6 +10,8 @@ namespace OnlineDataCrawler
     {
         static void Main(string[] args)
         {
+            AssemblyLoadContext.Default.Unloading += SigTermEventHandler; //register sigterm event handler. Don't forget to import System.Runtime.Loader!
+            Console.CancelKeyPress += CancelHandler; //register sigint event handler
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
             Logging.Log.Trace("Program start.");
             Console.WriteLine("Please input your Email address.");
@@ -18,6 +21,16 @@ namespace OnlineDataCrawler
             MainLoop loop = new MainLoop();
             loop.Init();
             loop.Loop();
+        }
+
+        private static void SigTermEventHandler(AssemblyLoadContext obj)
+        {
+            System.Console.WriteLine("Unloading...");
+        }
+
+        private static void CancelHandler(object sender, ConsoleCancelEventArgs e)
+        {
+            System.Console.WriteLine("Exiting...");
         }
     }
 }
